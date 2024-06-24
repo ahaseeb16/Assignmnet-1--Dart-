86ffa30fd8e0d9303dd05878df0c59b33e1e5802
Abdul Haseeb <143028440+ahaseeb16@users.noreply.github.com>
2024-06-24 21:47:23 +0500

    Q no 3

---
+++ Question no 3.dart
@@ -0,0 +1,10 @@
+// Q3
+void checkAttendance(int classesHeld, int classesAttended) {
+  double attendancePercentage = (classesAttended / classesHeld) * 100;
+  print('Attendance: ${attendancePercentage.toStringAsFixed(2)}%');
+  if (attendancePercentage >= 75) {
+    print('Student is allowed to sit in the exam.');
+  } else {
+    print('Student is not allowed to sit in the exam.');
+  }
+}
